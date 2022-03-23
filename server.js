f07c295b9a58c13a96e71989f552372a7148f3b1
Ousmane Fall <fallousmane162019@gmail.com>
2022-03-23 17:09:40 +0000

    message

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config({path: './.env'});
+
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+const prefix = "!";
+
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === "ping") {
+        message.reply(`Pong`);
+    }
+});
+
+client.login(process.env.CLIENT_TOKEN);
